3ad5773dfe515bfa8bc2387a68016d58b67911fa
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-05-08 22:26:10 -0700

    Scaffold astro-hash integration

---
+++ test/index.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, test } from 'vitest'
+import { createHash } from 'crypto';
+
+import { addSecurityAttributes } from '../src/index.ts';
+
+describe('Hash', () => {
+  const dir = '.';
+
+  test('Style', async () => {
+    const css = 'body { background-color: #ffffff; }';
+    const hash = createHash('sha256')
+      .update(css.trim())
+      .digest('base64');
+    const out = await addSecurityAttributes(`<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <style>${css}</style>
+        </head>
+        <body>
+          <p>Inline style test</p>
+        </body>
+      </html>`, dir);
+    expect(out).toContain(`integrity="sha256-${hash}"`);
+    expect(out).toContain(`crossorigin="anonymous"`);
+  });
+})
