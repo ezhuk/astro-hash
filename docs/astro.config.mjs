2994940c710077bf846cec667ff8d51553e28446
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-05-10 20:57:47 -0700

    Add docs and biome
    - Update README.md
    - Add biome
    - Add docs template
    - Add docs workflow

---
+++ docs/astro.config.mjs
@@ -0,0 +1,31 @@
+import starlight from '@astrojs/starlight';
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://ezhuk.github.io/astro-hash/',
+  base: '/astro-hash/',
+  integrations: [
+    starlight({
+      title: 'Astro Hash Docs',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/withastro/starlight',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [{ label: 'Example Guide', slug: 'guides/example' }],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
-import fs from 'fs/promises';
-import path from 'path';
-import { createHash } from 'crypto';
+import { createHash } from 'node:crypto';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
 export async function getFiles(dir: string): Promise<string[]> {
   const files: string[] = [];
   for (const entry of await fs.readdir(dir, { withFileTypes: true })) {
     const fullPath = path.join(dir, entry.name);
     if (entry.isDirectory()) {
-      files.push(...await getFiles(fullPath));
+      files.push(...(await getFiles(fullPath)));
     } else if (entry.isFile() && entry.name.endsWith('.html')) {
       files.push(fullPath);
     }
@@ -15,11 +15,16 @@ export async function getFiles(dir: string): Promise<string[]> {
   return files;
 }
 
-export async function readOrFetchFile(url: string, distDir: string): Promise<Buffer | null> {
+export async function readOrFetchFile(
+  url: string,
+  distDir: string,
+): Promise<Buffer | null> {
   try {
     if (/^(https?:)?\/\//.test(url)) {
       const fetchUrl = url.startsWith('//') ? `https:${url}` : url;
-      const res = await fetch(fetchUrl, { headers: { 'Cache-Control': 'no-cache' } });
+      const res = await fetch(fetchUrl, {
+        headers: { 'Cache-Control': 'no-cache' },
+      });
       if (!res.ok) return null;
       return Buffer.from(await res.arrayBuffer());
     }
@@ -31,7 +36,10 @@ export async function readOrFetchFile(url: string, distDir: string): Promise<Buf
   }
 }
 
-export function computeHash(data: Buffer | string, algorithm: 'sha256' | 'sha384'): string {
+export function computeHash(
+  data: Buffer | string,
+  algorithm: 'sha256' | 'sha384',
+): string {
   const hash = createHash(algorithm).update(data).digest('base64');
   return `${algorithm}-${hash}`;
 }
