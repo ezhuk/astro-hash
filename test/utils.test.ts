ac2eb8d00d46c0c3985801b8a48bbcd3f9834e03
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-07-13 14:45:34 -0700

    Add utils tests

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,9 +1,12 @@
 import { createHash } from 'node:crypto';
-import { describe, expect, test } from 'vitest';
+import fs from 'node:fs/promises';
+import os from 'node:os';
+import path from 'node:path';
+import { afterAll, beforeAll, describe, expect, test, vi } from 'vitest';
 
-import { computeHash } from '../src/utils.ts';
+import { computeHash, getFiles, readOrFetchFile } from '../src/utils.ts';
 
-describe('Hash', () => {
+describe('computeHash', () => {
   const data = 'test string 123';
 
   test('sha256', async () => {
@@ -20,5 +23,67 @@ describe('Hash', () => {
     expect(hash).toBe(
       `${alg}-${createHash(alg).update(data.trim()).digest('base64')}`,
     );
+  });
+});
+
+describe('getFiles', () => {
+  let tmpDir: string;
+
+  beforeAll(async () => {
+    tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'astro-hash-'));
+    await fs.writeFile(path.join(tmpDir, 'README.md'), '## README');
+    await fs.writeFile(path.join(tmpDir, 'index.html'), '<html></html>');
+    await fs.mkdir(path.join(tmpDir, 'sub'));
+    await fs.writeFile(path.join(tmpDir, 'sub', 'page.html'), '<html></html>');
+  });
+
+  afterAll(() => fs.rm(tmpDir, { recursive: true, force: true }));
+
+  test('html', async () => {
+    const files = await getFiles(tmpDir);
+    expect(files).toHaveLength(2);
+    expect(files).toEqual(
+      expect.arrayContaining([
+        path.join(tmpDir, 'index.html'),
+        path.join(tmpDir, 'sub', 'page.html'),
+      ]),
+    );
+  });
+});
+
+describe('readOrFetchFile', () => {
+  const localContent = 'local content';
+  const remoteContent = 'remote content';
+  let tmpDir: string;
+  let tmpFile: string;
+
+  beforeAll(async () => {
+    tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'astro-hash-'));
+    tmpFile = path.join(tmpDir, 'index.html');
+    await fs.writeFile(tmpFile, localContent, 'utf-8');
+
+    vi.stubGlobal('fetch', async (input: RequestInfo) => {
+      return {
+        ok: true,
+        arrayBuffer: async () => new TextEncoder().encode(remoteContent),
+      };
+    });
+  });
+
+  afterAll(() => {
+    vi.restoreAllMocks();
+    return fs.rm(tmpDir, { recursive: true, force: true });
+  });
+
+  test('local', async () => {
+    const buf = await readOrFetchFile(path.basename(tmpFile), tmpDir);
+    expect(buf).toBeInstanceOf(Buffer);
+    expect(buf.toString()).toBe(localContent);
+  });
+
+  test('remote', async () => {
+    const buf = await readOrFetchFile('//example.com/index.html', tmpDir);
+    expect(buf).toBeInstanceOf(Buffer);
+    expect(buf.toString()).toBe(remoteContent);
   });
 });
